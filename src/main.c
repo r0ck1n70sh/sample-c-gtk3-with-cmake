da80677f9beaafac9727a9783bbc516cfb8736ef
r0ck1n70sh <r0ck1n70sh@gmail.com>
2023-05-24 00:56:15 +0530

    initialized -- exampleapp

--- src/main.c
+++ src/main.c
@@ -1,27 +1,9 @@
 #include <gtk/gtk.h>
+#include "exampleapp.h"
 
-static void activate (
-		GtkApplication*	app,
-		gpointer	user_data) {
-
-	GtkWidget* window;
-
-	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "sample");
-	gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
-	gtk_widget_show_all(window);
-}
-
-int main (
-		int	argc,
-		char**	argv) {
-	GtkApplication* app;
-	int status;
-
-	app = gtk_application_new("org.r0ck1n70sh.sample-c-gtk", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-	status = g_application_run(G_APPLICATION(app), argc, argv);
-	g_object_unref(app);
-
-	return status;
+int main (int argc, char* argv[]) {
+        return g_application_run (
+                        G_APPLICATION (example_app_new ()),
+                        argc, 
+                        argv);
 }
