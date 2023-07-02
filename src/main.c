353ea84d09618d8157392afad941b9425045c7c3
r0ck1n70sh <r0ck1n70sh@gmail.com>
2023-07-02 13:50:08 +0530

    working commit -- 201

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include <gtk/gtk.h>
-#include "exampleapp.h"
+
+static void print_hello (GtkWidget * widget, gpointer data) {
+	g_print("hello\n");
+}
 
 int main (int argc, char* argv[]) {
-        return g_application_run (
-                        G_APPLICATION (example_app_new ()),
-                        argc, 
-                        argv);
+	GtkBuilder *builder;
+	GObject *window;
+	GObject *button;
+	GError *error = NULL;
+
+	gtk_init (&argc, &argv);
+
+	builder = gtk_builder_new ();
+	if (gtk_builder_add_from_file (builder, "ui/builder.ui", &error) == 0) {
+		g_printerr ("Error loading file: %s\n", error->message);
+		g_clear_error (&error);
+		return 1;
+	}
+	
+	window = gtk_builder_get_object (builder, "main-window");
+	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+	button = gtk_builder_get_object (builder, "button-1");
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+	button = gtk_builder_get_object (builder, "button-2");
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+	gtk_main();
+
+	return 0;
 }
